55e2919f5bfd6cf523aed35ad6370fb7b9460f07
OfryL <15746116+OfryL@users.noreply.github.com>
2024-11-09 18:22:06 +0200

    email api

---
+++ src/app/api/v1/emails/route.ts
@@ -0,0 +1,67 @@
+import { cookies } from 'next/headers'
+import { type NextRequest } from 'next/server'
+import Nylas, { ListMessagesQueryParams } from "nylas"
+
+const nylas = new Nylas({
+  apiKey: process.env.NYLAS_API_KEY,
+  apiUri: process.env.NYLAS_API_URI
+})
+
+const getParams = (forDate: string, pageToken: string | null = null) => {
+  let searchQueryNative = '';
+
+  const params: ListMessagesQueryParams = {
+    limit: 5,
+  }
+
+  if (pageToken) {
+    params.pageToken = pageToken
+  }
+
+  const today = forDate ? new Date(forDate) : new Date()
+  const theDayafter = new Date(today)
+  theDayafter.setDate(today.getDate() + 1)
+
+  searchQueryNative = searchQueryNative + ' after:' + today.toISOString().split('T')[0]
+  searchQueryNative = searchQueryNative + ' before:' + theDayafter.toISOString().split('T')[0]
+
+  params.searchQueryNative = searchQueryNative;
+
+  return params
+}
+
+async function fetchMails(grantId: any, forDate: string, pageToken: string | null = null) {
+  const params: ListMessagesQueryParams = getParams(forDate, pageToken);
+  const result = await nylas.messages.list({
+    identifier: grantId,
+    queryParams: params,
+  })
+
+  let data = result.data;
+  if (result.nextCursor) {
+    const moreData = await fetchMails(grantId, forDate, result.nextCursor);
+    data = [...result.data, ...moreData];
+  } else {
+    data = result.data;
+  }
+  return data;
+}
+
+export async function GET(request: Request & NextRequest) {
+  const cookieStore = cookies()
+
+  const cookie = cookieStore.get('nylas_user_grant_id')
+  const { value: grantId } = cookie as any;
+
+  const searchParams = request.nextUrl.searchParams;
+  const forDate = searchParams.get('forDate') as string
+
+  try {
+    const result = await fetchMails(grantId, forDate)
+    return Response.json({result})
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: 'Internal Server Error', inner: error }, { status: 500 })
+  }
+}
+
